566d09d179273a389abb1562bf8057eac0680480
JinhoSong <chosunz4@naver.com>
2021-03-16 12:36:20 +0900

    Initial commit

---
+++ src/test/java/io/security/coresecurity/lecture/LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.coresecurity.lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
